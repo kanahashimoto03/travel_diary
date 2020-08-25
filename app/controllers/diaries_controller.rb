322d047dc0e807110dffa00ce13a75175a88b4c0
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-25 13:52:07 +0900

    投稿ページの保存機能実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,8 +1,24 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
-
   def new
+    @diary = Diary.new
   end
-  
+
+  def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      redirect_to :root
+    else  
+      render :new 
+    end
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:country_id, :city, :category_id, :date, :recommendations, :things_to_avoide, :things_to_bring).merge(user_id: current_user.id)
+  end
+
 end
