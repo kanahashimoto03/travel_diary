f1aec1ac6ba56b8a587e3fdf1115e503eda50e5f
kanahashimoto03 <hka13ba077@icloud.com>
2020-08-27 16:29:55 +0900

    ransackによる検索画面の実装

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,13 +1,10 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :search_diary, only: [:index, :search]
 
   def index
-    @asia_diary = Diary.where(country_id: [1..4])
-    @europe_diary = Diary.where(country_id: [5..9])
-    @africa_diary = Diary.where(country_id: [10..14])
-    @oceania_diary = Diary.where(country_id: [15..17])
-    @north_america_diary = Diary.where(country_id: [18..20])
-    @south_america_diary = Diary.where(country_id: [21..24])
+    @diary = Diary.all
+    set_diary_column
   end
   
   def new
@@ -27,10 +24,23 @@ class DiariesController < ApplicationController
     @diary = Diary.find(params[:id])
   end
 
+  def search
+    @results = @q.result
+  end
 
   private
   def diary_params
     params.require(:diary).permit(:image, :country_id, :city, :category_id, :date, :recommendations, :things_to_avoide, :things_to_bring).merge(user_id: current_user.id)
+  end
+
+  private 
+  def search_diary
+    @q = Diary.ransack(params[:q])
+  end
+
+  def set_diary_column
+    @country_name = Diary.select("country_id").distinct
+    @category_name = Diary.select("category_id").distinct
   end
 
 end
