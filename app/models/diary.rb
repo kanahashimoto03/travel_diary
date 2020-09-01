7b03b45923e8d83e93573de9a782200fda0f59a5
kanahashimoto03 <hka13ba077@icloud.com>
2020-09-01 17:33:42 +0900

    バリデーションの設定

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -6,5 +6,19 @@ class Diary < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments
+
+  with_options presence: true do
+    validates :image
+    validates :country_id
+    validates :city
+    validates :category_id
+    validates :date
+    validates :recommendations
+  end
+
+  with_options numericality: { other_than: 1, message: 'Select'} do
+    validates :country_id
+    validates :category_id
+  end  
   
 end
