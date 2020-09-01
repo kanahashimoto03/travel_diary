03beb5719539e5c89bfe0c211bbbea1b60af6acc
kanahashimoto03 <hka13ba077@icloud.com>
2020-09-01 16:37:20 +0900

    選択肢の設定変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,9 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'place to stay' },
-    { id: 3, name: 'things to eat' },
-    { id: 4, name: 'things to do' },
-    { id: 5, name: 'things to see' }
+    { id: 2, name: 'place to stay(宿泊)' },
+    { id: 3, name: 'things to eat(食事)' },
+    { id: 4, name: 'things to do(やること)' },
+    { id: 5, name: 'things to see(観るもの)' }
   ]
 end
