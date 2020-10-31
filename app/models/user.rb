752ad560707ffdc172bad15d559e8dc70ad7bce3
kanahashimoto03 <hka13ba077@icloud.com>
2020-10-31 17:06:38 +0900

    ゲストログインの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
 
   has_many :diaries
   has_many :comments
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.nickname = "ゲストユーザー"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   get 'search', to:'diaries#search'
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
